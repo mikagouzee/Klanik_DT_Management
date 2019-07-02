99cc1e97e94d0f01b26fa8dca044ca3893aee238
MagBa <magos.barnabe@gmail.com>
2019-07-02 13:53:50 +0200

    Logger added

--- Klanik_Internal/Filters/GlobalValidation.cs
+++ Klanik_Internal/Filters/GlobalValidation.cs
@@ -1,15 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Klanik_Internal.LogMachines;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Klanik_Internal.Filters {
 
     public class ValidateModelFilter : ActionFilterAttribute {
+        public ILogMachine _logMachine { get; set; }
+
+        public ValidateModelFilter(ILogMachine logger)
+        {
+            _logMachine = logger;
+        }
         public override void OnActionExecuting(ActionExecutingContext context)
         {
-            if (!context.ModelState.IsValid)
+            try
             {
-                context.Result = new BadRequestObjectResult(context.ModelState);
+                if (!context.ModelState.IsValid)
+                {
+                    context.Result = new BadRequestObjectResult(context.ModelState);
+                }
             }
+            catch (System.Exception ex)
+            {
+
+                _logMachine.Log(ex.Message);
+                _logMachine.Log(ex.InnerException?.Message);
+            }
+
         }
     }
 
